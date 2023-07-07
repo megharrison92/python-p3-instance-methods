a736e03d4467eb2c52efa743a93cfc56ed21e5af
Megan Harrison <mtharrison@alumni.fullsail.edu>
2023-07-07 14:16:01 -0500

    lab completed

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
 
-    #Instance method definition
-    pass
+    def bark(self):
+        print("Woof!")
+    
+    def sit(sit):
+        print("The dog is sitting.")
